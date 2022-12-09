349924ea4b1f897a7b57673ade4271d0b41cbef0
bogdy234 <filimonbogdan89@gmail.com>
2022-12-09 20:23:32 +0200

    change remix config

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,7 @@ module.exports = {
   serverBuildTarget: "vercel",
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
+  future: {},
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
